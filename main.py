f7b6ff5063ea1145177439f978831c672a11e401
mahendra1290 <mahendra.suthar.1290@gmail.com>
2019-04-12 00:48:12 +0530

    login signup working about us linked

--- main.py
+++ main.py
@@ -1,30 +1,68 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request, flash, url_for, redirect, render_template
+from flask import Flask, request, flash, url_for, redirect, render_template, flash
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////media/maahi/Acer/Mahendra/webproject/develop.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://///media/maahi/Acer/Mahendra/web_development/Web_Develpoment/develop.db'
 app.config['SECRET_KEY'] = "#fuhafiuhd8r093820$%@!"
+app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
-class User(db.Model):
+class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    email    = db.Column(db.String(120), unique=True, nullable=False)
+    phone   = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __init__(self, name, email, phone, password):
+        self.username = name
+        self.email = email
+        self.phone = phone
+        self.password = password
 
 @app.route('/')
 def show_all():
    return render_template('layout.html')
 
-@app.route('/login')
+@app.route('/about')
+def about():
+    return render_template('aboutUs.html')
+
+@app.route('/login', methods = ['POST', 'GET'])
 def login():
+    if request.method == 'POST':          
+        username     = request.form['loginFeild']
+        password    = request.form['pwdFeild']
+        user = Users.query.filter_by(username = username).first()
+        if user is not None:
+            if check_password_hash(user.password, password):
+                flash("successfully logged in")
+            else:
+                flash("password is incorrect")
+        else:
+            flash("username incorrect")    
     return render_template('login.html')
 
-@app.route('/signup')
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+@app.route('/signup' ,methods = ['POST', 'GET'])
 def signup():
+    if request.method == 'POST':          
+        name     = request.form['uName']
+        email    = request.form['eMail']
+        phone    = request.form['phoneNum']
+        passw    = request.form['pwdFeild']
+        passw = generate_password_hash(passw)
+        user = Users(name, email, phone, passw)
+        db.session.add(user)
+        db.session.commit()
     return render_template('signup.html')
 
 if __name__ == '__main__':
-   app.run(debug = True)
+    app.run(debug = True)
     
--- static/js/main.js
+++ static/js/main.js
@@ -1,16 +1,15 @@
 function myFunction() {
-	if(document.getElementById('tagBox').style.display == 'block')
+	if (document.getElementById('tagBox').style.display == 'block')
 		document.getElementById('tagBox').style.display = 'none';
 
-	else
-	{
+	else {
 		document.getElementById('tagBox').style.display = 'block';
 	}
-	}	
+}
 function myFunction2(x) {
-  if (x.matches) { // If media query matches
-    document.getElementById('tagBox').style.display = "none";
-  } 
+	if (x.matches) { // If media query matches
+		document.getElementById('tagBox').style.display = "none";
+	}
 }
 
 var x = window.matchMedia("(min-width: 900px)")
@@ -21,21 +20,33 @@ function validateForm() {
 	var name = document.forms["myForm"]["uName"].value;
 	var num = document.forms["myForm"]["phoneNum"].value;
 	var email = document.forms["myForm"]["eMail"].value;
-	if(name=="")
+	if (name == "")
 		alert("Name must be filled out");
-	checkNum(num); 
+	checkNum(num);
 	validate(email);
 }
-function checkNum(num)
-{
+
+function validateLoginForm() {
+	var name = document.forms["loginForm"]["loginFeild"].value;
+	var pass = document.forms["loginForm"]["pwdFeild"].value;
+	if (name == "") {
+		alert("Name must be filled out");
+		return false
+	}
+	else if (pass == "") {
+		alert("password must be filled")
+		return false;
+	}
+	return true;
+}
+
+function checkNum(num) {
 	var mLen = num.length;
-	if(mLen!=10)
+	if (mLen != 10)
 		return alert("Please enter a valid number");
-	else
-	{
-		for(var i=0;i<10;i++)
-		{
-			if(num[i]<48 && num[i]>57)
+	else {
+		for (var i = 0; i < 10; i++) {
+			if (num[i] < 48 && num[i] > 57)
 				return alert("Please enter a valid number");
 		}
 	}
@@ -43,11 +54,29 @@ function checkNum(num)
 }
 function validate(email) {
 
-            var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
-            //var address = document.getElementById[email].value;
-            if (reg.test(email) == false) 
-            {
-                alert('Invalid Email Address');
-                return (false);
-            }
- }
+	var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+	//var address = document.getElementById[email].value;
+	if (reg.test(email) == false) {
+		alert('Invalid Email Address');
+		return (false);
+	}
+}
+
+//function to display message and then fade out it
+function Timer() {
+	var myVar = setInterval(myTimer, 20);
+	var d = 1;
+	function myTimer() {
+		d = d - .01;
+		if (d < 0) {
+			clearInterval(myVar)
+		}
+		if (document.getElementById("msg") == null) {
+			clearInterval(myVar)
+		}
+		else {
+			document.getElementById("msg").style.opacity = d;
+		}
+	}
+	myTimer();
+}
