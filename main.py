69a4c935f9f8b54cb9c7b0da295c4d6c0e0a8d78
mahendra1290 <mahendra.suthar.1290@gmail.com>
2019-04-11 22:35:15 +0530

    project cleaned

---
+++ main.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+from flask import Flask, request, flash, url_for, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////media/maahi/Acer/Mahendra/webproject/develop.db'
+app.config['SECRET_KEY'] = "#fuhafiuhd8r093820$%@!"
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+@app.route('/')
+def show_all():
+   return render_template('layout.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+if __name__ == '__main__':
+   app.run(debug = True)
+    
